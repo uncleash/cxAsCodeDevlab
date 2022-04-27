53a39c7fc879b851bd568a9abf1c50e4e4857cef
Travis Coleman <travis.coleman@genesys.com>
2022-04-27 11:46:13 -0400

    removed oauth info from variables file

--- infrastructure-as-code/variables.tf
+++ infrastructure-as-code/variables.tf
@@ -1,10 +1,10 @@
 variable "oauthclient_id" {
-  default = "8e1addf4-7ee7-4ccf-a54d-e7c26564fa31"
+  default = "insert here"
 }
 variable "oauthclient_secret" {
-  default = "Q4wrP50t4ut8PQ_xrotgsrzh2aC2_p4HheI39OyjNGk"
+  default = "insert here"
 }
 
 variable "aws_region" {
-  default = "DCA"
+  default = "insert here"
 }
